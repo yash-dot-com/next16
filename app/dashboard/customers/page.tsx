112ef7dce80449d6cfe04e3ed9df388213554c9d
yash sonalekar <yashislearningrn@gmail.com>
2026-06-27 09:30:56 +0530

    learnt a few things

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function CustomerPage(){
+    return(
+        <div>
+            <p>Customer Page</p>
+        </div>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function CustomerPage(){
+    return(
+        <div>
+            <p>Invoices Page</p>
+        </div>
+    )
+}
